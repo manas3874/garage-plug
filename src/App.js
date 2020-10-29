d09a9cfabc202ac47f6fba76516f969a6d851d92
Manas Tripathi <tripathimanas98@gmail.com>
2020-10-29 21:38:06 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import logo from "./logo.svg";
+// import "./App.css";
+import React from "react";
+import HomePage from "./components/HomePage";
+
+function App() {
+  return (
+    <div className="App">
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
